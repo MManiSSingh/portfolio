25275128bb4f0468286ae838e94dea9e8d4cc319
MManiSSingh <166468945+MManiSSingh@users.noreply.github.com>
2025-01-31 17:05:37 -0800

    Update global.js

--- global.js
+++ global.js
@@ -4,10 +4,34 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "resume.html", title: "Resume" },
+  { url: "https://github.com/MManiSSingh", title: "GitHub Profile" }
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
 
-currentLink?.classList.add("current");
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!ARE_WE_HOME && !url.startsWith("http")) {
+    url = "../" + url;
+  }
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle("current", a.host === location.host && a.pathname === location.pathname);
+
+  a.toggleAttribute("target", a.host !== location.host);
+
+  nav.append(a);
+}
